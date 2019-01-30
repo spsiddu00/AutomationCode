c68dcfc383f75604c68d54d19da438b554ea6e7b
shiddanagouda.patil <shiddanagouda.patil@accenture.com>
2019-01-30 19:22:19 +0530

    New Structure

---
+++ venv/Siddu/__init__.py
@@ -0,0 +1,12 @@
+
+
+##### Question ----> Read the Multiple files and Count the Occurance of a perticular Word in each File and Print the same
+
+
+##### Ex: ----> Read 3 files F1,F2,F3 and then Find the occurance of word 'Accenture' in each File and Print
+
+
+##### Expectation(OutPut): ---->
+#(File_1 , 2)
+#(File_2 , 5)
+#(File_3 , 0)
